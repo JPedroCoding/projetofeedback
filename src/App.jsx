18701fadfbb7668a27eeff97bed30e0e4e1a63a0
ribeirobritojoaopedro05 <ribeirobritojoaopedro05@gmail.com>
2025-04-14 04:20:39 +0000

    mudança da função handleSubmited para o CardInicial ; alterção da duração do hover dos botões

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { CardInicial } from "./components/Cardinicial"
+import { CardInicial } from "./components/CardInicial"
 import { CardFinal } from "./components/CardFinal"
 
 export function App(){
@@ -7,18 +7,17 @@ export function App(){
   const [submited, setSubmited] = useState(false)
   //O React precisa da função "useState" para atualizar ao na tela dinamicamente para o usuário e também usamos a função "setNotaAvaliacao" para mudar o valor da variável "notaAvaliacao" pois a função cons ultilizada no react é uma constante 
   
-
-
-function handleSubmit(){
-  notaAvaliacao !== 0 ?  (setSubmited(true)) : (setSubmited(false), alert("Por favor selecione uma nota antes de enviar")) 
-
+function handleMudarNotaAvaliacao(nota){
+  setNotaAvaliacao(nota)
+ //A variavél "nota" recebe o valor da nota do onclick e armazena isto na variável nota
 
 }
 
 
+
   return(
     submited === false ? (
-      <CardInicial setNotaAvaliacao={setNotaAvaliacao} handleSubmit={handleSubmit}/>
+      <CardInicial setNotaAvaliacao={setNotaAvaliacao} notaAvaliacao={notaAvaliacao} setSubmited={setSubmited}/>
      ) :  (
       <CardFinal notaAvaliacao={notaAvaliacao}/>
 
