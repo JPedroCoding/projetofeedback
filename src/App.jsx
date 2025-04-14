3b6b7a29f286ffe21a18ae588314e3b180d2cab7
ribeirobritojoaopedro05 <ribeirobritojoaopedro05@gmail.com>
2025-04-14 03:48:27 +0000

    mudança da função handleMudarNota para o componente input button ja que é responsabilidade dele

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,7 @@ export function App(){
   const [submited, setSubmited] = useState(false)
   //O React precisa da função "useState" para atualizar ao na tela dinamicamente para o usuário e também usamos a função "setNotaAvaliacao" para mudar o valor da variável "notaAvaliacao" pois a função cons ultilizada no react é uma constante 
   
-function handleMudarNotaAvaliacao(nota){
-  setNotaAvaliacao(nota)
- //A variavél "nota" recebe o valor da nota do onclick e armazena isto na variável nota
 
-}
 
 function handleSubmit(){
   notaAvaliacao !== 0 ?  (setSubmited(true)) : (setSubmited(false), alert("Por favor selecione uma nota antes de enviar")) 
@@ -22,7 +18,7 @@ function handleSubmit(){
 
   return(
     submited === false ? (
-      <CardInicial handleMudarNotaAvaliacao={handleMudarNotaAvaliacao} handleSubmit={handleSubmit}/>
+      <CardInicial setNotaAvaliacao={setNotaAvaliacao} handleSubmit={handleSubmit}/>
      ) :  (
       <CardFinal notaAvaliacao={notaAvaliacao}/>
 
